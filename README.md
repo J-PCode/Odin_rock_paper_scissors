c623558ca785d43d1c91b959cdf846fd7290e128
J-PCode <orjala.juho@gmail.com>
2022-01-02 12:00:54 +0200

    added new ui for this project, also reworked js, css, and index.html files.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Odin_rock_paper_scissors
 1st javascript project in odin project. Create rock paper scissors game where you will be playing 5 rounds against computer.
+
+Version 2
+Play until you or computer wins 5 rounds.
+
+there is 2 div in index.html. one show results after rock/paper/scissors is selected. when confirmed div is changed and another round begins. this is done with css display:none. css class is removed or added for the divider depending what part of the game is going.
+
+After player or Computer gets 5 points game ends. there is function for showing results and after that it resets points so player can play again if so decides.
+
+variables:
+computer
+player
+round
+playerSelection
+tulos
+computerSelection
+
+Functions:
+computerPlay: (random computer choise)
+playRound: (show results of the round (example rock beats paper))
+results: show results in results divider and toggle visibility. Also calls endGame function when game ends.
+nextRound: show buttons for rock,paper and scissors. also changes text for "game mode"
+reset: Resets game value after endGame is called.
+endGame: shows endGame results and calls reset function
+game: calls either results function or nextRound. This is done by listening what button is pressed.
+
+
+Some basic css also added.
--- main.js
+++ main.js
@@ -1,76 +1,128 @@
 const shapes =['rock', 'paper', 'scissors'];
 let computer = 0;
 let player = 0;
+let round = 1;
+let playerSelection;
+let tulos;
+let computerSelection = computerPlay();
+
 // tässä annetaan tietokoneen arvo
 function computerPlay(){
     const random = Math.floor(Math.random() * shapes.length);
+    console.log(random);
     return (shapes[random]);
     }
-    //käyttäjän valinta 3 näppäintä
-function playerSelect(){
-const buttons = document.querySelectorAll("button");
-buttons.forEach((button) =>{
-    button.addEventListener("click", () => {
-         playRound(button.id);//tähän lisätään se mitä tapahtuu kun nappia on painettu
 
-    })
-});    
-}
-
-//tässä otetaan erä, lisätään piste voittajalle ja kerrotaan tulos, voisihan tämän tulosten kertomisen jotenkin lyhyemmin varmaan tehdä... esim. console.log(´${playerSelection} vs ${computerSelection}, ${result})
-function playRound(playerSelection){
-//let playerSelection = playerSelect();
-let computerSelection = computerPlay();
-
-// tähän taitaa tulla sitten se tekstin lisäys sivustolle
+//tässä otetaan erä, lisätään piste voittajalle ja kerrotaan tulos, voisihan tämän tulosten kertomisen jotenkin lyhyemmin varmaan tehdä... 
+//esim. console.log(´${playerSelection} vs ${computerSelection}, ${result})
+function playRound(){
+computerSelection = computerPlay();
 if(playerSelection=='rock' && computerSelection == 'paper'){
+    round++;
     computer++
     return "You lose! Paper beats Rock";
 }
 if(playerSelection=='scissors' && computerSelection == 'paper'){
-player++;
+    round++;
+    player++;
 return "You win! Scissors cuts paper";
 }
 if(playerSelection=='paper' && computerSelection == 'paper'){
-return "Both choose paper, it's a tie!";
+    round++;
+    return "Both choose paper, it's a tie!";
 }
 if(playerSelection=='rock' && computerSelection == 'scissors'){
+    round++;
     player++;
-return "You win! Rock beats the scissors";
+    return "You win! Rock beats the scissors";
 }
 if(playerSelection=='scissors' && computerSelection == 'scissors'){
-return "Both chose scissors, it's a tie!";
+    round++;
+    return "Both chose scissors, it's a tie!";
 }
 if(playerSelection=='paper' && computerSelection == 'scissors'){
-    computer++
-return "You lose, Scissors beats the paper!";
+    round++;
+    computer++;
+    return "You lose, Scissors beats the paper!";
 }
 if(playerSelection=='rock' && computerSelection == 'rock'){
-return "It's a tie, rock vs rock";
+    round++;
+    return "It's a tie, rock vs rock";
 }
 if(playerSelection=='scissors' && computerSelection == 'rock'){
-    computer++
-return "You lose! Rock beats the scissors";
+    round++;
+    computer++;
+    return "You lose! Rock beats the scissors";
 }
 if(playerSelection=='paper' && computerSelection == 'rock'){
+    round++;
     player++;
     return "You win, paper beats the rock!";
 }
 }
-//game function tässä määritetään että pelataan 5 erää ja lopuksi ilmoitetaan voittaja. 
-// function Game(){
-//     for(i=1;i<=5;i++){
-//         console.log(playRound());
-//         }
-// if(player>computer){
-// console.log("congratulations you won the game");
-// }
-// else if(player == computer){
-//     console.log("game over, it was a tie!");
-// }
-// else{
-//     console.log("unfortunately you lose");
-// }
-// }
 
-//add button event listeners using forEach method
+function results(){
+    
+    document.querySelector(".header").textContent = `Round ${round}`;
+    document.querySelector("#tulos").textContent = playRound();
+    tulos = `Player ${playerSelection} vs ${computerSelection} Computer`;
+   
+    document.querySelector("#valitsi").textContent = `${tulos}`;
+    document.querySelector("#pisteet").textContent = `${player} - ${computer}`
+    document.querySelector("#begin").textContent = "Next Round";
+        document.querySelector("#game").classList.add("hide");
+    document.querySelector("#results").classList.remove("hide");
+    console.log("tulos", tulos)
+    if(playerSelection != "begin" && (player == 3 || computer == 3)){
+        endGame();    
+    }
+    }
+function nextRound(){
+    document.querySelector(".round").textContent = ``;
+    document.querySelector(".header").textContent = `Round ${round}`; 
+    document.querySelector(".info").textContent = "Player VS Computer";
+    document.querySelector("#game").classList.remove("hide");
+    document.querySelector("#results").classList.add("hide");
+    }
+
+function reset(){
+    
+    player = 0;
+    computer = 0;
+    round = 1;
+    
+}    
+function endGame(){   
+    if(player >= 3){
+        document.querySelector(".round").textContent = `Congratulations you won the game!!!`;
+        document.querySelector("#valitsi").textContent = `${tulos}`;
+        document.querySelector("#begin").textContent = "New Game"; 
+        reset();
+    }
+    else if(computer >= 3){
+        document.querySelector(".round").textContent = `Unfortunately you lose!!!`;
+        document.querySelector("#valitsi").textContent = `${tulos}`;
+        document.querySelector("#begin").textContent = "New Game";
+        reset();
+    }
+
+}
+function game(){
+if(playerSelection == "begin" && (player < 3 || computer < 3)){
+    nextRound();
+}
+
+else{
+    results();
+}
+}
+
+
+//käyttäjän valinta funktio 3 näppäintä
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) =>{
+    button.addEventListener("click", () => {
+            playerSelection = button.id;
+            game();
+    })
+}); 
